663e3c35907eafec5d3b56ad3335412fb25cc9cb
Dimitris Nakos <nakosd@gmail.com>
2026-08-22 00:51:06 -0400

    test1

--- kill_server.sh
+++ kill_server.sh
@@ -3,7 +3,7 @@
 echo "Terminating any running instances of yDyL Server and its proxies..."
 
 pkill -9 -f "server.py" 2>/dev/null
-pkill -9 -f "ngrok http" 2>/dev/null
+#pkill -9 -f "ngrok http" 2>/dev/null
 pkill -9 -f "cloud-sql-proxy" 2>/dev/null
 
 echo "Done."
